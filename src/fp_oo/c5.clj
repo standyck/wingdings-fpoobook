58fceda0460bfa210e50ea0779ac9fa02696ffe4
Stan Dyck <stan.dyck@gmail.com>
2012-11-09 07:42:08 -0800

    Exercise 1 complete

--- src/fp_oo/c5.clj
+++ src/fp_oo/c5.clj
@@ -1,17 +1,26 @@
 (ns fp-oo.c5)
 
 (def apply-message-to (fn [class instance message args]
-                        ))
+                        (let [method (message (:__instance_methods__ class))]
+                          (apply method instance args))))
 
 (def make (fn [class & args]
-            (let [seeded      {:__class_symbol__ (:__own_symbol__ class)}
-                  constructor (:add-instance-values (:__instance_methods__ class))]
-              (apply constructor seeded args))))
+            (let [seeded {:__class_symbol__ (:__own_symbol__ class)}]
+              (apply-message-to class seeded :add-instance-values args))))
 
 (def send-to (fn [instance message & args]
-               (let [class  (eval (:__class_symbol__ instance))
-                     method (message (:__instance_methods__ class))]
-                 (apply method instance args))))
+               (let [class (eval (:__class_symbol__ instance))]
+                 (apply-message-to class instance message args))))
+
+(def make-old (fn [class & args]
+                (let [seeded      {:__class_symbol__ (:__own_symbol__ class)}
+                      constructor (:add-instance-values (:__instance_methods__ class))]
+                  (apply constructor seeded args))))
+
+(def send-to-old (fn [instance message & args]
+                   (let [class  (eval (:__class_symbol__ instance))
+                         method (message (:__instance_methods__ class))]
+                     (apply method instance args))))
 
 (def Point
   {:__own_symbol__       'Point
