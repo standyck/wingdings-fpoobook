35b33dbe53d3bd93d2e3bb125911a5c515a0a5db
Stan Dyck <stan.dyck@gmail.com>
2012-11-10 20:27:09 -0800

    Added some chapter 5 exercise documentation.

--- src/fp_oo/c5.clj
+++ src/fp_oo/c5.clj
@@ -1,9 +1,8 @@
 (ns fp-oo.c5)
 
-;;Exercise 1
 (def apply-message-to (fn [class instance message args]
                         (let [method (or (message (:__instance_methods__ class))
-                                         message)]                               ;;Add or condition for execise 4
+                                         message)]                   ;;Add or condition for execise 4
                           (apply method instance args))))
 
 (def make (fn [class & args]
@@ -27,38 +26,38 @@
 
 (def Point
   {:__own_symbol__       'Point
-   :__instance_methods__
-   {:class-name          :__class_symbol__
-    :class               (fn [this] (eval (:__class_symbol__ this)))
-    :add-instance-values (fn [this x y]
-                           (assoc this :x x :y y))
-    :shift               (fn [this dx dy]
-                           (make Point
-                                 (+ (:x this) dx)
-                                 (+ (:y this) dy)))
-    :origin              (fn [this] (make Point 0 0))}})
+   :__instance_methods__ {:class-name          :__class_symbol__
+                          :class               (fn [this] (eval (:__class_symbol__ this)))
+                          :add-instance-values (fn [this x y]
+                                                 (assoc this :x x :y y))
+                          :shift               (fn [this dx dy]
+                                                 (make Point
+                                                       (+ (:x this) dx)
+                                                       (+ (:y this) dy)))
+                          :origin              (fn [this] (make Point 0 0))}})
 
 ;;exercise 3
 ;;before:
-;;(def p1 (make Point 1 2))
+;; (def p1 (make Point 1 2))
 ;; (send-to p1 :origin) => NullPointerException
 ;;... Add :origin function to Point...
 ;; (send-to p1 :origin) => {:y 0, :x 0, :__class_symbol__ Point}
-;; Since :origin is attached to Point (the class) and not the instance (p1), we can
+;; Since :origin is attached to the class (Point) and not the instance (p1), we can
 ;; modify the class and have those changes apply to existing instances. The send-to
 ;; function looks up the class each time so includes changes.
 
 ;;exercise 4 Here is Holder. See apply-messasge-to above.
 (def Holder
-  {:__own_symbol__ 'Holder
-   :__instance_methods__
-   {:add-instance-values (fn [this held] (assoc this :held held))}})
+  {:__own_symbol__        'Holder
+   :__instance_methods__  {:add-instance-values
+                           (fn [this held] (assoc this :held held))}})
 
 ;;before
 ;; (def h1 (make Holder "stuff"))
 ;; (send-to h1 :held) => NullPointerException
-;;after changes
+;;after changes to apply-message-to...
 ;; (send-to h1 :held) => "stuff"
 
 ;;exercise 5
 ;; (send-to h1 :some-unknown-message) => nil
+;; Since retrieval from a clojure map returns nil when the key doesn't exist.
